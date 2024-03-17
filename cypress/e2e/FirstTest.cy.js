0f903a44547fee7224deca36d5d742d9b2b6a79a
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-03-17 19:24:26 +0200

    Sing up sesting

--- cypress/e2e/FirstTest.cy.js
+++ cypress/e2e/FirstTest.cy.js
@@ -20,4 +20,22 @@ describe('template spec', () => {
     // Click the second button
     cy.get('button').eq(1).click();
   })
+  
+  it('Sign up testing', () => {
+    // Check the welcome message
+    cy.get('h1').should('contain.text', 'Welcome to Team Finder');
+
+    // Click the sign up button
+    cy.get('button:last').click();
+
+    // Enter user details
+    cy.get('input').eq(0).type('Admin Test'); // First input
+    cy.get('input').eq(1).type('teste_mail@example.com'); // Second input
+    cy.get('input').eq(2).type('Password123!'); // Third input
+    cy.get('input').eq(3).type('Error404 Corporation'); // Fourth input
+    cy.get('input').eq(4).type('Error404 Headquarters'); // Fifth input
+
+    cy.get('button').eq(1).click();
+
+});
 })
