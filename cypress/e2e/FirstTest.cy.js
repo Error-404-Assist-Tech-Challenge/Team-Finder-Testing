c950bd858d739fc1cfd0735e57bda1a6e605718f
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-03-17 19:12:36 +0200

    Log in testing

--- cypress/e2e/FirstTest.cy.js
+++ cypress/e2e/FirstTest.cy.js
@@ -1,5 +1,23 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://team-finder-404.web.app/myskills')
+  beforeEach(() => {
+    cy.visit('https://team-finder-404.web.app');
+  })
+
+  it('Log in testing', () => {
+    // Check the welcome message
+    cy.get('h1').should('contain.text', 'Welcome to Team Finder');
+
+    // Click the first button
+    cy.get('button:first').click();
+
+    // Enter email and password
+    cy.get('input:first').type('dudadud@yahoo.com');
+    cy.get('input:last').type('Password123!');
+
+    // Click the login button
+    cy.get('button:first').click();
+
+    // Click the second button
+    cy.get('button').eq(1).click();
   })
 })
